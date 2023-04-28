76b1b1b7d65fa249e6c6e1dcff55dceabe58b3e1
“Pablo <pronskypablo@gmail.com>
2023-04-28 19:44:47 -0300

    new pc, trying to connect

---
+++ Last-oop/Exercise6/src/Main.java
@@ -0,0 +1,12 @@
+import com.pablopronsky.Rectangle;
+
+public class Main {
+  public static void main(String[] args) {
+    Rectangle rectangle = new Rectangle(4, 6);
+    rectangle.calculateArea();
+  }
+}
+/*Crea una clase "Rectángulo" que tenga atributos "lado1" y "lado2" y un método
+"calcular_area" que calcule y devuelva el área del rectángulo.
+Luego crea un objeto "rectangulo1" de la clase "Rectángulo" con lados de 4 y 6
+y imprime el área del rectángulo.*/
---
+++ Last-oop/Exercise6/src/com/pablopronsky/Rectangle.java
@@ -0,0 +1,19 @@
+package com.pablopronsky;
+
+public class Rectangle {
+  int side1;
+  int side2;
+  // constructor
+  public Rectangle(int side1, int side2) {
+    this.side1 = side1;
+    this.side2 = side2;
+  }
+  //metodo
+  public void calculateArea(){
+    System.out.println(side1 * side2);
+  }
+}
+/*Crea una clase "Rectángulo" que tenga atributos "lado1" y "lado2" y un método
+"calcular_area" que calcule y devuelva el área del rectángulo.
+Luego crea un objeto "rectangulo1" de la clase "Rectángulo" con lados de 4 y 6 y
+imprime el área del rectángulo.*/
